9168566885f4f975b11eb6520f8e970ebe4ecec0
carthik7989 <cojective.karthik@gmail.com>
2025-12-19 13:32:51 +0530

    fixed button alignment

--- src/app/Components/PrimaryButton.tsx
+++ src/app/Components/PrimaryButton.tsx
@@ -9,8 +9,8 @@ export default function PrimaryButton({ children, className }: PrimaryButtonProp
     const url = "https://pages.razorpay.com/ai-course"
 
     return (
-        <Link href={url}>
-            <button className={`${className} px-5 py-3 md:px-11 md:py-4 text-sm md:text-base xxl:text-lg text-white font-open-sans font-semibold rounded-full primary cursor-pointer`}>{children}</button>
-        </Link>
+
+        <button className={`${className} px-5 py-3 md:px-11 md:py-4 text-sm md:text-base xxl:text-lg text-white font-open-sans font-semibold rounded-full primary cursor-pointer`}><Link href={url}>{children}</Link></button>
+
     );
 }   
